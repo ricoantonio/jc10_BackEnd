afa4f1c552faa4021508c766775343aeb390f1a6
ricoantonio <ricoantonio33@hotmail.com>
2019-10-07 10:13:59 +0700

    C:/Program Files/Git/gettoken

--- token/index.js
+++ token/index.js
@@ -1,11 +1,13 @@
 //npm i express body-parser cors mysqljs/mysql jsonwebtoken
 
+var jwt =  require('jsonwebtoken')
 var express = require('express')
 var app = express()
 var bodyParser = require('body-parser')
 var cors=require('cors')
-const port=7070
+const port=8080
 const db=require("./database/index")
+const appKey = 'secretkey'
 
 app.use(bodyParser.json())
 app.use(cors())
@@ -14,5 +16,21 @@ app.get("/",(req,res)=>{
     res.send(`<h1>Welcome to my first API!</h1>`)
 })
 
+app.get('verifytoken',(req,res)=>{
+
+})
+
+app.post('/gettoken',(req,res)=>{
+    let {username,email}=req.body
+    let token = jwt.sign({username, email}, appKey, {expiresIn:'12h'})
+    console.log(token)
+
+    res.send({
+        username,
+        email,
+        token
+    })
+    
+})
 
 app.listen(port,()=>console.log(`Listening from ${port}`))
